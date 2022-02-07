de1abfdecab63fabe15708e4de016844f8e38d4d
Ashika555 <83506752+Ashika555@users.noreply.github.com>
2022-02-07 13:11:50 +0530

    Add files via upload

---
+++ build/precache-manifest.918e9e5538e3167c999d8578e4033ff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28221b49aaf105bdf02996665bac690a",
+    "url": "/thetodoapp/index.html"
+  },
+  {
+    "revision": "9b07c523aadcd2a8edbe",
+    "url": "/thetodoapp/static/css/main.e0f0cf8d.chunk.css"
+  },
+  {
+    "revision": "60cbdcb988246dadc326",
+    "url": "/thetodoapp/static/js/2.bb71aa13.chunk.js"
+  },
+  {
+    "revision": "9b07c523aadcd2a8edbe",
+    "url": "/thetodoapp/static/js/main.26cca0ad.chunk.js"
+  },
+  {
+    "revision": "15a0432c42fa1e71cafe",
+    "url": "/thetodoapp/static/js/runtime~main.9a7dcef8.js"
+  }
+]);
